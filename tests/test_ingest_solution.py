b2545ed61d74d31d0520ed2d677cc26ac655b79c
Axionis47 <sidsy04@gmail.com>
2026-03-19 22:30:09 -0400

    test(ingest): update seed count assertion to 26 after new victories added

--- tests/test_ingest_solution.py
+++ tests/test_ingest_solution.py
@@ -122,7 +122,7 @@ def test_ensure_seeds_loaded_uses_tenex_delivered(monkeypatch, tmp_path):
 
     store = ChromaStore(collection_name="tenex_delivered")
     store._init_chroma()
-    assert store._collection.count() == 20
+    assert store._collection.count() == 26
 
     # ai_solutions collection should NOT have been created
     import chromadb
