d41eca55bb53d7d33cd0136acc1db7dabfb31acd
Axionis47 <sidsy04@gmail.com>
2026-03-23 17:01:25 -0400

    feat(thought): add package exports for ThoughtEngine and result types

---
+++ engines/thought/__init__.py
@@ -0,0 +1,4 @@
+from engines.thought.reasoning_loop import ThoughtEngine
+from core.schemas import ReasoningLoopResult, ReasoningState, UserQuestion
+
+__all__ = ["ThoughtEngine", "ReasoningLoopResult", "ReasoningState", "UserQuestion"]
